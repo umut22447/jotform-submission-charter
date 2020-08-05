514e363e1442fae746ef0de42df12aa7f8626a76
Umut <61562205+umut22447@users.noreply.github.com>
2020-08-05 18:23:53 +0300

    Add files via upload

--- src/Util.js
+++ src/Util.js
@@ -3,32 +3,32 @@ export const fillDataArray = (answers, field) => {
     var answerText = "";
     answers.forEach(a => {
         console.log(a[field].text + " sorusuna gelen cevap : " + a[field].answer);
-        let indexOfAnswer = isAnswerExist(dataArr, a[field].answer);
-        if (indexOfAnswer !== -1) {
-            dataArr[indexOfAnswer][1]++;    //Increase the answer count in the array if the answer is already exists in array.
-        }
-        else {
-            if (a[field].answer) {
-                if (typeof (a[field].answer) === "object") {    //If the field is multiple choice box.
-                    a[field].answer.map(a => {
-                        let index = isAnswerExist(dataArr, a);
-                        if (index !== -1) {
-                            dataArr[index][1]++;
-                        }
-                        else {
-                            let newData = [a, 1];
-                            dataArr.push(newData);
-                        }
-                    })
+        if (typeof (a[field].answer) === "object") {    //If the field is multiple choice box.
+            a[field].answer.map(a => {
+                let index = isAnswerExist(dataArr, a);
+                if (index !== -1) {
+                    dataArr[index][1]++;
                 }
                 else {
+                    let newData = [a, 1];
+                    dataArr.push(newData);
+                }
+            })
+        }
+        else {                                             //If the field is not multiple choice box.
+            let indexOfAnswer = isAnswerExist(dataArr, a[field].answer);
+            if (indexOfAnswer !== -1) {
+                dataArr[indexOfAnswer][1]++;    //Increase the answer count in the array if the answer is already exists in array.
+            }
+            else {
+                if (a[field].answer) {                      //If the answer is not empty.
                     let newData = [a[field].answer, 1];     //Creates a new answer with count 1 and push it to dataArr
                     dataArr.push(newData);
                 }
 
             }
-
         }
+
         answerText = a[field].text;
     })
 

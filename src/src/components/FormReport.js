ecf2d8703082d88cf6314d4ee3b6e92482a9be65
Umut <61562205+umut22447@users.noreply.github.com>
2020-08-10 14:39:35 +0300

    Add files via upload

---
+++ src/src/components/FormReport.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Chart from './Chart'
+import { useReport } from '../contexts/ReportContext'
+
+export default function FormReport() {
+
+    const { form, report } = useReport();
+
+    /*const handleAddClick = (field) => {
+        addChartByField(field);
+    }*/
+
+    return (
+        <div>
+            <h1>Answer Reports for <strong className='text-info'>{form.title}</strong></h1><br />
+
+            <br />
+            {report.map(r => {
+                return (<Chart report={r} />)
+            })}
+
+        </div>
+    )
+}
---
+++ src/src/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { AuthProvider } from './contexts/AuthContext'
+import { FormsProvider } from './contexts/FormsContext'
+import FormList from './components/FormList'
+import FormReportPage from './components/FormReportPage';
+import SearchBox from './components/SearchBox'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+import { Navbar, Nav } from 'react-bootstrap';
+import logo from './images/logo.png'
+
+
+ReactDOM.render(
+  <AuthProvider>
+    <FormsProvider>
+      <Router>
+        <Navbar bg="dark" variant="dark" sticky='top'>
+          <Navbar.Brand>
+            <img
+              src={logo}
+              width="25"
+              height="25"
+              className="d-inline-block align-top"
+              alt="Jotform logo"
+            />{' '}
+            JotForm Submission Charter
+          </Navbar.Brand>
+          <Nav className='mr-auto'>
+            <Nav.Link href='/formlist'>Form List</Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route path="/formlist">
+            <div>
+              <h2 className='d-flex justify-content-center'>Select a Form and See the Charter</h2>
+              <SearchBox />
+            </div>
+            <FormList />
+          </Route>
+          <Route path="/report/:formId" children={<FormReportPage />}>
+          </Route>
+        </Switch>
+      </Router>
+    </FormsProvider>
+  </AuthProvider>,
+  document.getElementById('root')
+);

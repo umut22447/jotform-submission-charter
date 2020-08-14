3a8bd1a1573f0f76eb5e588edbe457ea8191ee6b
Umut <umuttvural@gmail.com>
2020-08-14 13:25:24 +0300

    Changed some refs with event

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,8 +5,6 @@ import { useReport } from '../contexts/ReportContext'
 
 export default function Chart(props) {
     const divRef = useRef();
-    const chartTypeSelectRef = useRef();
-    const dateSelectRef = useRef();
     const { answers, deleteChartByField, changeChartTypeByField, changeDateByField, submissions } = useReport();
     const { field, title, chartType, date } = props.report;
     const handleClick = () => {
@@ -24,24 +22,24 @@ export default function Chart(props) {
         }
     }, [answers, field, divRef, title, chartType, date, submissions]);
 
-    const handleChartTypeChange = () => {
-        const selectedValue = chartTypeSelectRef.current.value;
+    const handleChartTypeChange = (event) => {
+        const selectedValue = event.target.value;
         changeChartTypeByField(field, selectedValue);
     }
 
-    const handleDateChange = () => {
-        const dateValue = dateSelectRef.current.value;
+    const handleDateChange = (event) => {
+        const dateValue = event.target.value;
         changeDateByField(field, dateValue);
     }
 
     return (
         <div className=''>
             <strong>{title}</strong>
-            <select ref={chartTypeSelectRef} onChange={handleChartTypeChange} value={chartType}>
+            <select onChange={handleChartTypeChange} value={chartType}>
                 <option>Pie</option>
                 <option>Line</option>
             </select>
-            <select ref={dateSelectRef} onChange={handleDateChange} value={date}>
+            <select onChange={handleDateChange} value={date}>
                 <option value='All'>All</option>
                 <option value='last7days'>Last 7 Days</option>
                 <option value='last3month'>Last 3 Month</option>
